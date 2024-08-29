395c104a928b9ba849dc7c8b27c44f16eb0e3400
SaymonSilveiradaCruz <saymonscruz@gmail.com>
2024-08-29 19:41:54 -0300

    Estagio_gupy

--- desafio_02.py
+++ desafio_02.py
@@ -1,11 +1,9 @@
-# Definindo a string
-minha_string = "Abracadabra"
+def count_letter_a(string):
+    count = string.lower().count('a')
+    return count
 
-# Contando as ocorrências de 'a' e 'A'
-contagem_a = minha_string.count('a') + minha_string.count('A')
+test_string = "Abracadabra"
 
-# Verificando a existência e imprimindo o resultado
-if contagem_a > 0:
-    print(f"A letra 'a' aparece {contagem_a} vezes na string.")
-else:
-    print("A letra 'a' não aparece na string.")
+
+count = count_letter_a(test_string)
+print(f"A letra 'a' aparece {count} vezes na string '{test_string}'.")
